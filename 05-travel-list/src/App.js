82aceaaac1c6ad673fd99aab666acbf70aac207e
aracho <byaracho@gmail.com>
2024-07-25 22:47:51 +0900

    fix: form

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -22,10 +22,11 @@ function Logo() {
 function Form() {
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(e);
   }
 
   return (
-    <div className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😉 trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
@@ -36,7 +37,7 @@ function Form() {
       </select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>
-    </div>
+    </form>
   );
 }
 
