c8fef447c4506f3df16d1bbb539cbd814a6fbe30
aracho <byaracho@gmail.com>
2024-07-25 22:31:24 +0900

    fix: PackingList

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -12,6 +17,7 @@ export default function App() {
 function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
@@ -19,9 +25,30 @@ function Form() {
     </div>
   );
 }
+
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.description} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item(item) {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
